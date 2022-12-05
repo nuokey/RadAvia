d26932c4b600df7ccc2f89442938045644808d9c
Timo <66961498+nuokey@users.noreply.github.com>
2022-12-05 11:51:07 +0300

    Update

--- map/main.py
+++ map/main.py
@@ -1,7 +1,26 @@
-from urllib.request import urlopen
-import json
-import requests
-import pandas as pd
-from selenium import webdriver
-from bs4 import BeautifulSoup as bs
-import plotly.graph_objects as go
+import folium
+
+data = open("map/data.txt").read();
+
+data = data.split("\n")
+for i in range(len(data)):
+    data[i] = data[i].split()
+    for q in range(len(data[i])):
+        data[i][q] = float(data[i][q])
+
+print(data)
+
+m = folium.Map(location=[data[0][0], data[0][1]], zoom_start=16)
+
+for dot in data:
+    color = "purple";
+    if dot[2] <= 10:
+        color="green";
+    elif dot[2] >= 10 and dot[2] <= 20:
+        color="yellow";
+    elif dot[2] >= 20:
+        color="red";
+    # elif dot[2]
+    folium.Circle(radius=100, location=[dot[0], dot[1]], color=color, popup=dot[2], fill=True).add_to(m)
+
+m.save('name.html')
